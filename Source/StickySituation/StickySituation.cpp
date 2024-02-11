1ce91349d00428a432f8efe4285d89eab048c73e
noisfischer <noisfischer@gmail.com>
2024-02-11 11:32:43 -0500

    Initial commit

---
+++ Source/StickySituation/StickySituation.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StickySituation.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StickySituation, "StickySituation" );
+ 
---
+++ Source/StickySituation/StickySituationGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "StickySituationGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AStickySituationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AStickySituationGameMode();
+};
+
+
+
