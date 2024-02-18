a6a57d726204083f964ae453685d6e14dd5702bd
Joseph Clay <joe@weareimmutable.com>
2024-02-18 10:16:11 -0600

    web: deps & start of jwt

--- web/packages/constants/index.ts
+++ web/packages/constants/index.ts
@@ -6,9 +6,14 @@ export const isProd = process.env.NODE_ENV === 'production';
 export const VOLUME_NAME = process.env.RAILWAY_VOLUME_NAME || '.ss';
 export const VOLUME_PATH = process.env.RAILWAY_VOLUME_MOUNT_PATH || '/.ss';
 
-export const SECRET = process.env.SECRET || '';
+export const SECRET = process.env.SECRET || '376ea5af478b9b6b90119f454548b0795f47965ddad86291922212817f42992ab579fdc90df9b52ca6f517694598e615c12afad46eaad4d2285326aa589a2ad84540c61960b5aad068e8f50cad5ae7c85826a0ef70c270a52fb33af2a0b9b4c1cf71b5880354191e6c6da08c90e4aae2438a93cf3f8642427b58dc655fdfab306bdce2967ccd0ff5b63273ef9a2e4ae5576b827d3f41cc96a08d18375cb63cb3de9e0ff73e68dde8044d7a9dd3bc80821a859ff87eab910884ede3334a31b9945fc49b2ec4ee6440a3bed50c9df122e870a45aafeb172bbcd0655760cebdb938b76608981cfd3404f065116502cef28f5207cfaa50137de1d33550ae2c250249';
+export const TOKEN_DURATION = 60; // in seconds
 
 export const base = process.env.BASE || '/';
+
+export type Profile = {
+  id: string;
+};
 
 export enum CHARACTER {
   JEREMY = 1,
--- web/src/api/security/index.ts
+++ web/src/api/security/index.ts
@@ -1,22 +1,33 @@
 import jwt from 'jsonwebtoken';
-import { SECRET } from '@ss/constants';
+import { type Request } from 'express';
+import {
+  SECRET,
+  TOKEN_DURATION,
+  
+  type Profile,
+} from '@ss/constants';
 
-export const payloadToToken = (payload: Profile): Promise<string | null> => {
+export const parseAuthorizationHeader = (req: Request) => {
+  const { authorization = '' } = req.headers;
+  return authorization.split(' ').at(1) ?? '';
+};
+
+export const payloadToToken = (payload: Profile): Promise<string | undefined> => {
   return new Promise(resolve => {
-    jwt.sign(payload, SECRET, {}, (err, token) => {
+    jwt.sign(payload, SECRET, { expiresIn: TOKEN_DURATION }, (err, token) => {
       if (err) {
         console.error(err);
-        return resolve(null);
+        return resolve(undefined);
       }
       resolve(token);
     });
   });
 };
 
-export const tokenToPayload = async function(token: string): Promise<Profile | null> {
+export const tokenToPayload = async function(token: string): Promise<Profile | undefined> {
   return new Promise(resolve => {
-    jwt.verify(token, SECRET, { ignoreExpiration: true }, (err, decoded) => {
-      if (err) return resolve(null);
+    jwt.verify(token, SECRET, (err, decoded) => {
+      if (err) return resolve(undefined);
       resolve(decoded as Profile);
     });
   });
