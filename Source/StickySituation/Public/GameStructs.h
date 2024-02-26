90e50d314c65407b0da5f0680ff36e714361d822
noisfischer <noisfischer@gmail.com>
2024-02-26 16:37:33 -0500

    ProjectileAttributes struct init

---
+++ Source/StickySituation/Public/GameStructs.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameStructs.generated.h"
+
+USTRUCT(Blueprintable)
+struct FProjectileAttributes
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ProjectileDamage = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ProjectileSpeed = 0;
+};
