8615d93d58d34c0b96f2aa5209cc8f496922dab8
noisfischer <noisfischer@gmail.com>
2024-03-07 17:08:06 -0500

    Discarded Unused Enum

--- Source/StickySituation/Public/GameEnums.h
+++ Source/StickySituation/Public/GameEnums.h
@@ -1,12 +1,3 @@
 ﻿#pragma once
 
 #include "CoreMinimal.h"
-
-UENUM(BlueprintType)
-enum class EAmmoType : uint8
-{
-	Red  UMETA(DisplayName = "Red"),
-	Green  UMETA(DisplayName = "Green"),
-	Blue  UMETA(DisplayName = "Blue"),
-	Yellow  UMETA(DisplayName = "Yellow"),
-};
